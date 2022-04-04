c853b19729c378f48f91636a94d7d15627f3de41
Christopher <100327670+ChristopherMara@users.noreply.github.com>
2022-04-04 00:04:24 +0000

    Component 1a

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
 
-def ask_name(): #everything is stored in a function for easier assembly
+def ask_name(): #everything is stored in a function for easier assembly of the final program
   print("Welcome to my maths quiz!")
   global name
-  name = input("What is your name?")
+  name = input("What is your name? \n > ")
+  print("Hello, " + name + "!")
+
+ask_name()
+
+
+
+
+
