2552b6f5a6a05c933e023e8522ebbb012eea8eb8
Christopher <100327670+ChristopherMara@users.noreply.github.com>
2022-04-04 00:29:09 +0000

    Component 1b: add .isalpha()

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 
 
+import math
+
+def print_fancy(decoration, text): #prints lines with decorations
+  print(decoration*math.ceil(len(text)/len(decoration)))
+  print(text)
+  print(decoration*math.ceil(len(text)/len(decoration)))
+  
 def ask_name(): #everything is stored in a function for easier assembly of the final program
-  print("Welcome to my maths quiz!")
+  print_fancy("-=-","Welcome to my maths quiz!")
   global name
-  name = input("What is your name? \n > ")
-  print("Hello, " + name + "!")
+  while True:
+    name = input("\nWhat is your name? \n > ")
+    if name.isalpha():
+      break
+    else:
+      print("Invalid name, please only enter letters")
+  print("\nHello, " + name + "! \nThis is a multiplication and division maths quiz \nYou can decide the length and starting difficulty \nIf you allow it to, the quiz will gradually get harder \nThe time it takes for you to answer each question will also be timed")
+  
 
 ask_name()
 
