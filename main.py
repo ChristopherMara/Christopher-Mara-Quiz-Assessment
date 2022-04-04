215d4bfe6ba755bb93e569baeda2f67f0beb92a1
Christopher <100327670+ChristopherMara@users.noreply.github.com>
2022-04-04 22:49:26 +0000

    Component 1c: added .replace()

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def ask_name(): #everything is stored in a function for easier assembly of the f
   print_fancy("-=-","Welcome to my maths quiz!")
   global name
   while True:
-    name = input("\nWhat is your name? \n > ")
-    if name.isalpha():
+    name = input("\nWhat is your name? \n > ").strip()
+    if name.replace(" ","").isalpha():
       break
     else:
       print("Invalid name, please only enter letters")
-  print("\nHello, " + name + "! \nThis is a multiplication and division maths quiz \nYou can decide the length and starting difficulty \nIf you allow it to, the quiz will gradually get harder \nThe time it takes for you to answer each question will also be timed")
+  print("\nHello, " + name + "! \nThis is a multiplication and division maths quiz. \nYou can decide the length and starting difficulty. \nIf you allow it to, the quiz will gradually get harder. \nThe time it takes for you to answer each question will also be timed.")
   
 
 ask_name()
